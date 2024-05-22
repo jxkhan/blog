ac6e7d297618c6d19b16fa613729ff346668fc0c
Junaid Khan <junaidsajidkhan@gmail.com>
2024-05-22 21:03:21 +0500

    feat: lint errors fixed

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const express = require('express')
-const app = express()
-const routes = require('./routes/routes.js')
+const express = require("express");
+const app = express();
+const routes = require("./routes/routes.js");
 
-app.use(express.json( ))  
+app.use(express.json());
 
-app.use("/api/v1/", routes)
+app.use("/api/v1/", routes);
 
 try {
-    app.listen(3000, () => {
-      console.log('Server started on port 3000');
-    });
-  } catch (error) {
-    console.error('Error starting server:', error);
-    process.exit(1);
-  }
-
-
-
+  app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
+} catch (error) {
+  console.error("Error starting server:", error);
+  process.exit(1);
+}

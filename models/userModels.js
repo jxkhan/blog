a4ffae9d92a70fd0a197042148ed714e50ec7d8b
Junaid Khan <junaidsajidkhan@gmail.com>
2024-05-27 16:32:18 +0500

    feat(3.1.2): user model created

---
+++ models/userModels.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../index");
+const bcrypt = require("bcrypt");
+
+const Users = sequelize.define(
+  "users",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      set(value) {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(value, salt);
+        this.setDataValue("password", hash);
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+
+  {
+    timestamps: false,
+  }
+);
+
+Users.sync()
+  .then((data) => {
+    console.log("User Table and Model synced Successfully", data);
+  })
+  .catch((err) => {
+    console.log("Error syncing User Table and Model !!", err);
+  });
+
+module.exports = Users;
